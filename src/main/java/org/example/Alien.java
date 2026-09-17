c67ad65e61bc31e6ce0d4d929b4b837691a71344
Shashi Singh <shashisingh25494@gmail.com>
2026-09-17 10:03:06 +0530

    Refrence injection in spring framework

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -3,6 +3,7 @@ package org.example;
 public class Alien {
 
     private int age;
+    private Laptop lap;  // WE HAVE THE REFERENCE OF LAPTOP CLASS HERE
     public Alien() {
         System.out.println("Object Created");
     }
@@ -16,7 +17,18 @@ public class Alien {
         this.age = age;
     }
 
+    // GETTER SETTER FOR THE LAPTOP VARIABLE
+    // TO WORK THIS WE NEED TO CONNECT WITH OBJECT IN SPRING.XML FILE
+    // WITH PROPERTY
+    public Laptop getLap() {
+        return lap;
+    }
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
+        lap.compile();
         System.out.println("Coding");
     }
 
