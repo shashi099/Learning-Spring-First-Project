1f3bc0f6a10d4707a1c0336c21d22109082d090d
Shashi Singh <shashisingh25494@gmail.com>
2026-09-17 09:44:55 +0530

    Scopes and setter injection in spring framework

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,8 +1,19 @@
 package org.example;
 
 public class Alien {
+
+    private int age;
     public Alien() {
         System.out.println("Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+//        System.out.println("setter injection");
+        this.age = age;
     }
 
     public void code(){
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,14 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Alien obj = (Alien) context.getBean("alien");
-//        obj.code();
+        Alien obj1 = (Alien) context.getBean("alien");
+        obj1.code();
+        System.out.println(obj1.getAge());
+
+
+//        Alien obj2 = (Alien) context.getBean("alien");
+//        obj2.code();
+//        System.out.println(obj2.age);
+
     }
 }
